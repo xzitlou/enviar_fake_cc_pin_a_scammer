f1bf7e3d8d696cd6e2c3737dd17482c5a255864c
Lou <hola@loualcala.com>
2019-11-04 14:54:34 -0500

    send fake document request

--- app.py
+++ app.py
@@ -18,12 +18,30 @@ def generate_random_pin():
     return ''.join(str(random.randint(0, 9)) for i in range(0, 6))
 
 
+def generate_random_dni():
+    rand_dni_sufix = [str(random.randint(0, 9)) for c in range(0, 7)]
+    card_number = '4%s' % ''.join(rand_dni_sufix)
+
+    return card_number
+
+
+print('== Sending data to scammer ==')
 for i in range(10000):
     scammer_url = 'https://ameriquote.com/bcpzonasegurabetaenlinea.vialbcp.com/iniciar-sesion.php'
-    data = {
+    card_data = {
         'cardNumber': generate_random_card(),
         'vecla': generate_random_pin()
     }
-    response = requests.post(scammer_url, data=data)
-    print(data)
-    print(response)
+    print('# Sending card info:', card_data)
+    r1 = requests.post(scammer_url, data=card_data)
+    print(r1)
+
+    dni_scammer_url = 'https://ameriquote.com/bcpzonasegurabetaenline.vialbcp/operacionesLinea/ajaxProcesos2.php'
+    doc_data = {
+        'Dni': generate_random_dni(),
+        'token': generate_random_pin(),
+        'op': 'DniToken'
+    }
+    print('# Sending document info:', doc_data)
+    r2 = requests.post(dni_scammer_url, data=doc_data)
+    print(r2)
